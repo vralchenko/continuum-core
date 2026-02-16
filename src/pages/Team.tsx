7fc573f7be510c896848b0427eced234e02d583c
Viktor Ralchenko <viktorr@finera.com>
2026-02-16 18:27:47 +0100

    feat: add linkedin profiles to founders

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -16,16 +16,31 @@ const Team: React.FC = () => {
                         <img src={innaImg} alt="Dr. Inna Praxmarer" className="founder-img" />
                         <h3>Dr. Inna Praxmarer</h3>
                         <span>Co-Founder</span>
+                        <div className="founder-social">
+                            <a href="https://www.linkedin.com/in/dr-inna-praxmarer/" target="_blank" rel="noopener noreferrer" className="social-link">
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+                            </a>
+                        </div>
                     </div>
                     <div className="founder">
                         <img src={olgaImg} alt="Olga Sushchinskaya" className="founder-img" />
                         <h3>Olga Sushchinskaya</h3>
                         <span>Co-Founder</span>
+                        <div className="founder-social">
+                            <a href="https://www.linkedin.com/in/sushchinskaya-olga-7ba85a5a/" target="_blank" rel="noopener noreferrer" className="social-link">
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+                            </a>
+                        </div>
                     </div>
                     <div className="founder">
                         <img src={viktorImg} alt="Viktor Ralchenko" className="founder-img" />
                         <h3>Viktor Ralchenko</h3>
                         <span>Co-Founder</span>
+                        <div className="founder-social">
+                            <a href="https://www.linkedin.com/in/victoralchenko/" target="_blank" rel="noopener noreferrer" className="social-link">
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
